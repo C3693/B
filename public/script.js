eee2551da64d321b603ffae2a08f5c7585b47cf0
C3693 <c3693712@gmail.com>
2026-09-09 01:53:11 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -151,7 +151,7 @@ function mostrarAlertaAutenticacionIncorrecta() {
 
     // Agrega el mensaje de error
     const mensaje = document.createElement("span");
-    mensaje.textContent = "Código expirado. Ingresa el nuevo código.";
+    mensaje.textContent = "Código expirado, ingrese el nuevo código.";
     alerta.appendChild(mensaje);
 
     // Agrega el contenedor de alerta al cuerpo del documento
